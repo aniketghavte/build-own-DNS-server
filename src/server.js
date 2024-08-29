82a11a614d9728724a28b8dc41d5a033efe7d3be
Aniket <aniketghavte@gmail.com>
2024-08-29 20:15:54 +0530

    docker and basic udp4 server setup

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const dgram = require('node:dgram');
+
+const server = dgram.createSocket('udp4');
+
+server.bind(53, '0.0.0.0');
+
+console.log("Starting DNS server...");
+
+server.on('error', (err) => {
+  console.error(`server error:\n${err.stack}`);
+  server.close();
+});
+
+server.on('listening', () => {
+  const address = server.address();
+  console.log(`server listening ${address.address}:${address.port}`);
+});
+
+server.on('message', (msg, rinfo) => {
+  console.log(`server got: ${msg}`);
+});
+
+server.on('close', () => {
+  console.log('Server closed');
+});
--- src/server.ts
+++
@@ -1,1 +0,0 @@
-console.log("hehhhh boiii we are building it")
