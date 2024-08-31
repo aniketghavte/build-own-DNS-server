395dd28d50022e0c1b11c6d240c972c155965add
Aniket <aniketghavte@gmail.com>
2024-08-31 09:39:28 +0530

    dns basics and functions setup

--- src/server.js
+++ src/server.js
@@ -1,14 +1,38 @@
 const dgram = require('node:dgram');
-
 const server = dgram.createSocket('udp4');
+
+const { decode, encode } = require('./dnsCore');
 
 server.bind(53, '0.0.0.0');
 
 console.log("Starting DNS server...");
 
 server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg}`);
+  const packet = decode(msg);
+  // console.log("buffer", buffer);
+  console.log("packet", packet);
+
+  // console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+  const response = encode({
+    type: 'response',
+    id: packet.id,
+    flags: 0x8180,
+    questions: packet.questions,
+    answers: [{
+      type: 'A',
+      class: 'IN',
+      name: packet.questions[0].name,
+      data: '1.1.1.1',
+    }]
+  })
+
+  server.send(response, rinfo.port, rinfo.address, (err) => {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
+
 server.on('error', (err) => {
   console.error(`server error:\n${err.stack}`);
   server.close();
